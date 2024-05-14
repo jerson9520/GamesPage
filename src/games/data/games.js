9e54bfd52a4ebd4ba86cfa92231304b7fccf3120
Jerson Martinez <1995jerson@gmail.com>
2024-05-14 09:59:44 -0500

    data games

---
+++ src/games/data/games.js
@@ -0,0 +1,162 @@
+export const games = [
+    {
+        'id': 'game-minecraft',
+        'name': 'Minecraft',
+        'publisher': 'Mojang Studios',
+        'genre': 'Sandbox, Survival',
+        'release_date': 'November 18, 2011',
+        'platforms': 'PC, Console, Mobile'
+    },
+    {
+        'id': 'game-fortnite',
+        'name': 'Fortnite',
+        'publisher': 'Epic Games',
+        'genre': 'Battle Royale, Sandbox, Survival',
+        'release_date': 'July 21, 2017',
+        'platforms': 'PC, Console, Mobile'
+    },
+    {
+        'id': 'game-cod-warzone',
+        'name': 'Call of Duty: Warzone',
+        'publisher': 'Activision',
+        'genre': 'Battle Royale, First-Person Shooter',
+        'release_date': 'March 10, 2020',
+        'platforms': 'PC, Console'
+    },
+    {
+        'id': 'game-zelda-botw',
+        'name': 'The Legend of Zelda: Breath of the Wild',
+        'publisher': 'Nintendo',
+        'genre': 'Action-Adventure',
+        'release_date': 'March 3, 2017',
+        'platforms': 'Nintendo Switch, Wii U'
+    },
+    {
+        'id': 'game-ac-valhalla',
+        'name': 'Assassin\'s Creed Valhalla',
+        'publisher': 'Ubisoft',
+        'genre': 'Action Role-Playing',
+        'release_date': 'November 10, 2020',
+        'platforms': 'PC, Console'
+    },
+    {
+        'id': 'game-cyberpunk-2077',
+        'name': 'Cyberpunk 2077',
+        'publisher': 'CD Projekt',
+        'genre': 'Action Role-Playing',
+        'release_date': 'December 10, 2020',
+        'platforms': 'PC, Console'
+    },
+    {
+        'id': 'game-red-dead-2',
+        'name': 'Red Dead Redemption 2',
+        'publisher': 'Rockstar Games',
+        'genre': 'Action-Adventure',
+        'release_date': 'October 26, 2018',
+        'platforms': 'PC, Console'
+    },
+    {
+        'id': 'game-gta-v',
+        'name': 'Grand Theft Auto V',
+        'publisher': 'Rockstar Games',
+        'genre': 'Action-Adventure',
+        'release_date': 'September 17, 2013',
+        'platforms': 'PC, Console'
+    },
+    {
+        'id': 'game-hades',
+        'name': 'Hades',
+        'publisher': 'Supergiant Games',
+        'genre': 'Roguelike, Dungeon Crawler',
+        'release_date': 'September 17, 2020',
+        'platforms': 'PC, Console'
+    },
+    {
+        'id': 'game-overwatch',
+        'name': 'Overwatch',
+        'publisher': 'Blizzard Entertainment',
+        'genre': 'First-Person Shooter',
+        'release_date': 'May 24, 2016',
+        'platforms': 'PC, Console'
+    },
+    {
+        'id': 'game-apex-legends',
+        'name': 'Apex Legends',
+        'publisher': 'Electronic Arts',
+        'genre': 'Battle Royale, First-Person Shooter',
+        'release_date': 'February 4, 2019',
+        'platforms': 'PC, Console, Mobile'
+    },
+    {
+        'id': 'game-horizon-zero-dawn',
+        'name': 'Horizon Zero Dawn',
+        'publisher': 'Sony Interactive Entertainment',
+        'genre': 'Action Role-Playing',
+        'release_date': 'February 28, 2017',
+        'platforms': 'PC, Console'
+    },
+    {
+        'id': 'game-witcher-3',
+        'name': 'The Witcher 3: Wild Hunt',
+        'publisher': 'CD Projekt',
+        'genre': 'Action Role-Playing',
+        'release_date': 'May 19, 2015',
+        'platforms': 'PC, Console'
+    },
+    {
+        'id': 'game-dark-souls-3',
+        'name': 'Dark Souls III',
+        'publisher': 'Bandai Namco Entertainment',
+        'genre': 'Action Role-Playing',
+        'release_date': 'March 24, 2016',
+        'platforms': 'PC, Console'
+    },
+    {
+        'id': 'game-sekiro',
+        'name': 'Sekiro: Shadows Die Twice',
+        'publisher': 'Activision',
+        'genre': 'Action-Adventure',
+        'release_date': 'March 22, 2019',
+        'platforms': 'PC, Console'
+    },
+    {
+        'id': 'game-ghost-tsushima',
+        'name': 'Ghost of Tsushima',
+        'publisher': 'Sony Interactive Entertainment',
+        'genre': 'Action-Adventure',
+        'release_date': 'July 17, 2020',
+        'platforms': 'PlayStation 4, PlayStation 5'
+    },
+    {
+        'id': 'game-final-fantasy-vii-remake',
+        'name': 'Final Fantasy VII Remake',
+        'publisher': 'Square Enix',
+        'genre': 'Action Role-Playing',
+        'release_date': 'April 10, 2020',
+        'platforms': 'PlayStation 4, PlayStation 5, PC'
+    },
+    {
+        'id': 'game-persona-5',
+        'name': 'Persona 5',
+        'publisher': 'Atlus',
+        'genre': 'Role-Playing',
+        'release_date': 'September 15, 2016',
+        'platforms': 'PlayStation 3, PlayStation 4'
+    },
+    {
+        'id': 'game-elden-ring',
+        'name': 'Elden Ring',
+        'publisher': 'Bandai Namco Entertainment',
+        'genre': 'Action Role-Playing',
+        'release_date': 'February 25, 2022',
+        'platforms': 'PC, Console'
+    },
+    {
+        'id': 'game-god-of-war',
+        'name': 'God of War',
+        'publisher': 'Sony Interactive Entertainment',
+        'genre': 'Action-Adventure',
+        'release_date': 'April 20, 2018',
+        'platforms': 'PlayStation 4, PlayStation 5, PC'
+    }
+]
